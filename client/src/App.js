a31f9ff529daf8477e28f99575177012e3fe3e02
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-02 17:40:28 +0100

    Add routing and first steps of basic page setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
-// import logo from "./assets/icon/fabstr.png";
-import { LoadingScreen } from "./pages/";
-import { HomePage } from "./pages/";
-import { ListPage } from "./pages/";
-import { CalenderPage } from "./pages/";
-import { MoodPage } from "./pages/";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
+import { LoadingScreen } from "./pages/LoadingScreen";
+import { HomePage } from "./pages/HomePage";
+import { ListPage } from "./pages/ListPage";
+import { CalenderPage } from "./pages/CalenderPage";
+import { MoodPage } from "./pages/MoodPage";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -22,13 +21,13 @@ function App() {
         <Route exact path="/">
           {loading ? <LoadingScreen /> : <HomePage />}
         </Route>
-        <Route exact path="./list">
+        <Route exact path="/list">
           <ListPage />
         </Route>
-        <Route exact path="./calender">
+        <Route exact path="/calender">
           <CalenderPage />
         </Route>
-        <Route exact path="./mood">
+        <Route exact path="/mood">
           <MoodPage />
         </Route>
       </Switch>
