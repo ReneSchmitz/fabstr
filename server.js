9372d13b2d92444ee347a6aaf8671f99c9f6b9ca
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-09 15:02:35 +0100

    Setup JSON server

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -11,8 +15,11 @@ app.use(
   express.static(path.join(__dirname, "client/storybook-static"))
 );
 
+server.use(middlewares);
+server.use("/api", router);
+
 app.get("*", (request, response) => {
-  response.sendFile(path.join(__dirname, "index.html"));
+  response.sendFile(path.join(__dirname, "client/public", "index.html"));
 });
 
 app.listen(port, () => {
