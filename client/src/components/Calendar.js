f16534ea081adfec0544b69d7f40316b36359ff6
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-09 10:45:48 +0100

    Delete simple-react-calender

--- client/src/components/Calendar.js
+++
@@ -1,9 +0,0 @@
-import SimpleReactCalendar from "simple-react-calendar";
-
-export default function Calendar() {
-  return (
-    <>
-      <SimpleReactCalendar activeMonth={new Date()} />
-    </>
-  );
-}
--- client/src/stories/Calendar.stories.js
+++
@@ -1,8 +0,0 @@
-import Calendar from "../components/Calendar";
-
-export default {
-  title: "Component/SimpleCalendar",
-  component: Calendar,
-};
-
-export const Simple_Calendar = () => <Calendar />;
