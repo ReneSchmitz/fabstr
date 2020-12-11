866b6621502a1f673bedcd28e11dc8d9d0b682c0
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-11 20:48:08 +0100

    Shorten URL in api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,31 +1,23 @@
 export async function getFruits() {
-  const response = await fetch(
-    `http://localhost:3001/api/groceries?type=fruit`
-  );
+  const response = await fetch(`/api/groceries?type=fruit`);
   const fruits = await response.json();
   return fruits;
 }
 
 export async function getDrinks() {
-  const response = await fetch(
-    `http://localhost:3001/api/groceries?type=drink`
-  );
+  const response = await fetch(`/api/groceries?type=drink`);
   const drinks = await response.json();
   return drinks;
 }
 
 export async function getVegetables() {
-  const response = await fetch(
-    `http://localhost:3001/api/groceries?type=vegetable`
-  );
+  const response = await fetch(`/api/groceries?type=vegetable`);
   const vegetables = await response.json();
   return vegetables;
 }
 
 export async function getCereals() {
-  const response = await fetch(
-    `http://localhost:3001/api/groceries?type=cereal`
-  );
+  const response = await fetch(`/api/groceries?type=cereal`);
   const cereals = await response.json();
   return cereals;
 }
