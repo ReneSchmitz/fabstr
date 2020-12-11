e33c63fb720a10b05d59e0c976c4bfb6650edd53
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-11 15:03:29 +0100

    Use get requests to render content from api

--- client/src/components/GroceriesList.js
+++ client/src/components/GroceriesList.js
@@ -1,20 +1,62 @@
 import { useEffect, useState } from "react";
-import { getGroceries } from "../utils/api";
+import { getFruits, getDrinks, getVegetables, getCereals } from "../utils/api";
 
 export default function GroceriesList() {
-  const [grocery, setGrocery] = useState(null);
+  const [fruit, setFruit] = useState(null);
+  const [drink, setDrink] = useState(null);
+  const [vegetable, setVegetable] = useState(null);
+  const [cereal, setCereal] = useState(null);
 
   useEffect(() => {
     async function doGet() {
-      const grocery = await getGroceries();
-      setGrocery(grocery);
-      console.log(grocery);
-      console.log(grocery.title, grocery.name);
+      const fruit = await getFruits();
+      setFruit(fruit);
+    }
+    doGet();
+  }, []);
+
+  useEffect(() => {
+    async function doGet() {
+      const drink = await getDrinks();
+      setDrink(drink);
+    }
+    doGet();
+  }, []);
+
+  useEffect(() => {
+    async function doGet() {
+      const vegetable = await getVegetables();
+      setVegetable(vegetable);
+    }
+    doGet();
+  }, []);
+
+  useEffect(() => {
+    async function doGet() {
+      const cereal = await getCereals();
+      setCereal(cereal);
     }
     doGet();
   }, []);
 
   return (
-    <>{grocery && grocery.title?.map((fruits) => <div key={fruits}></div>)}</>
+    <>
+      {fruit &&
+        fruit.map((foodItem) => <div key={foodItem.type}>{foodItem.name}</div>)}
+      <br />
+      {drink &&
+        drink.map((foodItem) => <div key={foodItem.type}>{foodItem.name}</div>)}
+      <br />
+      {vegetable &&
+        vegetable.map((foodItem) => (
+          <div key={foodItem.type}>{foodItem.name}</div>
+        ))}
+      <br />
+      {cereal &&
+        cereal.map((foodItem) => (
+          <div key={foodItem.type}>{foodItem.name}</div>
+        ))}
+      <br />
+    </>
   );
 }
--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,31 @@
-export async function getGroceriesHeader() {
-  const result = await fetch(`http://localhost:3001/api/groceries`);
-  const groceriesHeader = result.json();
-  return groceriesHeader;
+export async function getFruits() {
+  const response = await fetch(
+    `http://localhost:3001/api/groceries?type=fruit`
+  );
+  const groceries = await response.json();
+  return groceries;
 }
 
-export async function getGroceries() {
-  const result = await fetch(`http://localhost:3001/api/groceries`);
-  const groceries = result.json();
+export async function getDrinks() {
+  const response = await fetch(
+    `http://localhost:3001/api/groceries?type=drink`
+  );
+  const groceries = await response.json();
+  return groceries;
+}
+
+export async function getVegetables() {
+  const response = await fetch(
+    `http://localhost:3001/api/groceries?type=vegetable`
+  );
+  const groceries = await response.json();
+  return groceries;
+}
+
+export async function getCereals() {
+  const response = await fetch(
+    `http://localhost:3001/api/groceries?type=cereal`
+  );
+  const groceries = await response.json();
   return groceries;
 }
