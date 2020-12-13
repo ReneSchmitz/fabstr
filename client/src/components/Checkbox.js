5fdcb81762f277e10596cb8050ba567a5641dcc1
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-13 15:09:49 +0100

    Implementing improvment suggestions

--- client/src/components/Checkbox.js
+++ client/src/components/Checkbox.js
@@ -3,15 +3,15 @@ import Checkbox from "../assets/icon/checkbox.svg";
 import CheckboxChecked from "../assets/icon/checkboxChecked.svg";
 
 export default function GetCheckBox() {
-  const [ischecked, setIsChecked] = useState(false);
-  const CheckboxSrc = !ischecked ? Checkbox : CheckboxChecked;
-  const CheckboxAlt = !ischecked ? "Checkbox" : "Checkbox is ticked";
+  const [isChecked, setIsChecked] = useState(false);
+  const CheckboxSrc = !isChecked ? Checkbox : CheckboxChecked;
+  const CheckboxAlt = !isChecked ? "Checkbox" : "Checkbox is ticked";
 
   return (
     <img
       Src={CheckboxSrc}
       Alt={CheckboxAlt}
-      onClick={() => setIsChecked(!ischecked)}
+      onClick={() => setIsChecked(!isChecked)}
     />
   );
 }
