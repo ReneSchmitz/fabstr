161e2ba94b6b8114768b7294931149f0877a43cb
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-19 13:38:26 +0100

    Create wrappers

---
+++ client/src/components/DailyCardSlider.js
@@ -0,0 +1,17 @@
+import styled from "styled-components/macro";
+
+const Container = styled.div``;
+
+const SectionSlider = styled.section``;
+
+const SlideHolder = styled.div``;
+
+export default function Slider() {
+  return (
+    <Container>
+      <SectionSlider>
+        <SlideHolder></SlideHolder>
+      </SectionSlider>
+    </Container>
+  );
+}
