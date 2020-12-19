ac7dc26e2876066f6750d0dfb61a02372df6ea2c
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-19 13:44:35 +0100

    implement radio buttons

--- client/src/components/DailyCardSlider.js
+++ client/src/components/DailyCardSlider.js
@@ -6,10 +6,17 @@ const SectionSlider = styled.section``;
 
 const SlideHolder = styled.div``;
 
+const SliderRadio = styled.input``;
+
 export default function Slider() {
   return (
     <Container>
       <SectionSlider>
+        <SliderRadio type="radio" name="slider" id="slide-1" />
+        <SliderRadio type="radio" name="slider" id="slide-2" />
+        <SliderRadio type="radio" name="slider" id="slide-3" checked />
+        <SliderRadio type="radio" name="slider" id="slide-4" />
+        <SliderRadio type="radio" name="slider" id="slide-5" />
         <SlideHolder></SlideHolder>
       </SectionSlider>
     </Container>
