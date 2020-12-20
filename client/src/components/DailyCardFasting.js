d6af33fc2b19331fe130fc8287652b025b73bfe6
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-20 15:57:26 +0100

    Write content for fasting card

--- client/src/components/DailyCardFasting.js
+++ client/src/components/DailyCardFasting.js
@@ -18,11 +18,19 @@ const Header = styled.div`
   margin-bottom: 15px;
 `;
 
-const Content = styled.div`
+const TextContainer = styled.div`
   display: grid;
   overflow: auto;
   height: 52vh;
   padding-right: 20px;
+`;
+
+const Headline = styled.p`
+  padding-bottom: 8px;
+`;
+
+const Content = styled.span`
+  padding-bottom: 20%;
 `;
 
 export default function Preparation() {
@@ -33,20 +41,20 @@ export default function Preparation() {
           <p>Fastentage</p>
           <span>Tag 3-7</span>
         </Header>
-        <Content>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
-            dolorum enim earum excepturi molestiae odio voluptatibus libero
-            soluta, fugiat sequi nemo ab eveniet incidunt veritatis sit. Tenetur
-            error nam id?
-          </span>
-          <span>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
-            dolorum enim earum excepturi molestiae odio voluptatibus libero
-            soluta, fugiat sequi nemo ab eveniet incidunt veritatis sit. Tenetur
-            error nam id?
-          </span>
-        </Content>
+        <TextContainer>
+          <Headline>Morgens</Headline>
+          <Content>Kräutertee</Content>
+          <Headline>Vormittags</Headline>
+          <Content>Wasser und Tees</Content>
+          <Headline>Mittags</Headline>
+          <Content>Gemüsebrühe</Content>
+          <Headline>Nachmittags</Headline>
+          <Content>
+            Früchte- oder Kräutertee, Zitronenschnitz, 1tl ungesüßtes Quittenmus
+          </Content>
+          <Headline>Abends</Headline>
+          <Content>Verdünnte Säfte oder Gemüsebrühe</Content>
+        </TextContainer>
       </Container>
     </>
   );
