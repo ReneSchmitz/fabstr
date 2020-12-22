9341bbc87255658b287723af8303cb5422f98a73
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-12-22 23:18:48 +0100

    Change loading time

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setTimeout(() => setLoading(false), 5000);
+    setTimeout(() => setLoading(false), 4000);
   }, []);
 
   return (
