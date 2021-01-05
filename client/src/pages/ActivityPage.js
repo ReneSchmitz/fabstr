fb7cdb429adbe74029b3548f8ae668d46835ba4e
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2021-01-05 17:22:52 +0100

    Add header image and motivation quote

--- client/src/pages/ActivityPage.js
+++ client/src/pages/ActivityPage.js
@@ -1,5 +1,7 @@
 import styled from "styled-components/macro";
 import logo from "../assets/icon/fabstr.png";
+import image from "../assets/img/oceanTwo.jpg";
+import quote from "../assets/icon/quoteRight.svg";
 import Navigation from "../components/Navigation";
 
 const Container = styled.div`
@@ -13,6 +15,39 @@ const Container = styled.div`
   background: #eff2f8;
 `;
 
+const Frame = styled.div`
+  opacity: 0.7;
+  position: fixed;
+  top: 0;
+  max-height: 30%;
+  overflow: hidden;
+  z-index: 1;
+  img {
+    max-width: 100%;
+    position: relative;
+    top: -5em;
+  }
+`;
+
+const Quote = styled.div`
+  display: grid;
+  width: 80%;
+  img {
+    position: relative;
+    top: -30px;
+    display: flex;
+    justify-content: flex-end;
+  }
+  p {
+    margin-bottom: 5%;
+  }
+  span {
+    font-family: "Bodoni 72", serif;
+    font-weight: 800;
+    font-size: 1.5em;
+  }
+`;
+
 const Logo = styled.img`
   display: flex;
   flex-direction: column;
@@ -23,14 +58,33 @@ const Logo = styled.img`
   width: 40%;
 `;
 
-const Title = styled.h1`
-  font-size: 3.5rem;
+const Title = styled.h2`
+  font-size: 2em;
+  font-weight: 800;
+  position: fixed;
+  top: 9%;
+  display: grid;
+  left: 2.4em;
+  z-index: 2;
+  color: #ffffff;
+  text-shadow: 20px 20px 60px #303030, -20px -20px 60px #424040;
 `;
 
 export const ActivityPage = () => {
   return (
     <Container>
       <Title>Activity</Title>
+      <Frame>
+        <img src={image} alt="" />
+      </Frame>
+      <Quote>
+        <p>Today&lsquo;s insiration</p>
+        <img src={quote} alt="quote" />
+        <span>
+          Nothing behind me, <br />
+          everything ahead of me.
+        </span>
+      </Quote>
       <Logo src={logo} alt="logo" />
       <Navigation activePath="/activity" />
     </Container>
